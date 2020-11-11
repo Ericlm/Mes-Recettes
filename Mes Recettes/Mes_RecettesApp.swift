e6b517d1c676dbe91ea5395d9532511ba917dee4
Éric Le Maître <contact@ericlemaitre.fr>
2020-11-11 14:21:30 +0100

    Initial Commit

---
+++ Mes Recettes/Mes_RecettesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mes_RecettesApp.swift
+//  Mes Recettes
+//
+//  Created by Eric Le Maître on 11/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct Mes_RecettesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
